30b11c2f26e75cd6b25f4a9c7d30738245d076ae
Thiago <thiagowirzdemoura@gmail.com>
2021-12-12 22:26:09 -0300

    Add losePage

--- src/components/Lose.js
+++ src/components/Lose.js
@@ -1,5 +1,17 @@
-export default function Lose(){
+import "../css/losePage.css"
+import miniLogo from "../assets/logo-mini.png"
+import emoji from "../assets/sad.png"
+import nextArrow from "../assets/next.png"
+export default function Lose({setPage}){
     return(
-       <> miajuda</>
-    )
-}
+            <div className="lose-page">
+              <header>
+                <img src={miniLogo} alt="" />
+              </header>
+                <h1>Putz..<img src={emoji} alt= ""/></h1> 
+                <span>Você esqueceu 2 flashcards..<br/>Não desanime! Na próxima você consegue!</span>
+                <button onClick={() => setPage("home")}>Tentar Novamente! <img src = {nextArrow} alt = ""/></button>
+            </div>
+          );
+        }
+
